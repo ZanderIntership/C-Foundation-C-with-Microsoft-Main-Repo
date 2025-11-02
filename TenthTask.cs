2ce60b829fce323f3c44db29f464367ef7cee1c5
ZanderIntership <ZanderWepener5@gmail.com>
2025-11-02 11:09:55 +0200

    Create TenthTask.cs

---
+++ TenthTask.cs
@@ -0,0 +1,63 @@
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+
+switch (product[0])
+{
+
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat Pants";
+        break;
+    default:
+        type = "other";
+        break;
+}
+
+
+switch (product[1])
+{
+
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "white";
+        break;
+}
+
+
+switch (product[2])
+{
+
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size FIts All";
+        break;
+    }
+
+
+Console.WriteLine($"Product: {size} {color} {type}");
